8bdbe7492efb6c18a34a1e0d59af0392fc28a6c4
'Eric <eric@kolotyluk.net>
2014-04-12 16:41:16 -0700

    Delete old projects

--- elevate/Properties/AssemblyInfo.cs
+++ elevate/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Elevate.exe")]
-[assembly: AssemblyDescription("placeholder")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kreative Koncepts")]
 [assembly: AssemblyProduct("Open Source")]
